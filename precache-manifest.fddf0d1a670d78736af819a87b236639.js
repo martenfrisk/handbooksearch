413d0d3f36670a4d30f5df2324961cf8ec8fe737
Marten <marten.frisk@gmail.com>
2020-07-10 20:35:21 +0200

    Updates

---
+++ precache-manifest.fddf0d1a670d78736af819a87b236639.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18a3f99acf15605537c209a45f6df7bd",
+    "url": "/handbooksearch/index.html"
+  },
+  {
+    "revision": "15396873a762a2dbec1c",
+    "url": "/handbooksearch/static/css/main.d2d7cd36.chunk.css"
+  },
+  {
+    "revision": "dd2ff0da422ff9bd09c4",
+    "url": "/handbooksearch/static/js/2.515fcf02.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/handbooksearch/static/js/2.515fcf02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15396873a762a2dbec1c",
+    "url": "/handbooksearch/static/js/main.bf741755.chunk.js"
+  },
+  {
+    "revision": "8d097fbb9554ac880943",
+    "url": "/handbooksearch/static/js/runtime-main.1736a8e7.js"
+  }
+]);
