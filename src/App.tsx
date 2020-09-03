3eb8b4ac0ef80c3ebdf1d8b10ba22ffe18696ca2
Marten <marten.frisk@gmail.com>
2020-09-03 16:07:28 +0200

    Add shutdown announcement

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React, { ReactElement } from 'react'
-import { HashRouter as Router, Route, Redirect } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 
-import Search from './components/search'
+// import Search from './components/search'
 import Layout from './components/layout'
-import Ep from './components/ep'
+// import Ep from './components/ep'
 import './styles/main.css'
 
 function App(props: any): ReactElement {
@@ -12,7 +12,11 @@ function App(props: any): ReactElement {
 			<Router>
 				<Layout />
 				<div className="container w-full px-2 mx-auto md:px-10 lg:w-1/2">
-					<Route exact path="/">
+					<h1 className="w-full mb-20 text-3xl text-center">'Hollywood Handbook search' has shut down due to cost-related reasons.</h1>
+					<p className="mb-10">You can download the raw transcripts formatted for Elasticsearch here. You can easily use something like Notepad++ to search the file.</p>
+					<p>All lines (50mb json): <a className="underline font-blue-800" href="https://mega.nz/file/vfhVEZYZ#6LyDl6JKXb2t6jogKzyNE-J5Izyi2BDy0r4fwTeP69U" target="_blank" rel="noopener noreferrer">https://mega.nz/file/vfhVEZYZ#6LyDl6JKXb2t6jogKzyNE-J5Izyi2BDy0r4fwTeP69U</a></p>
+					<p>All episode titles (20mb json): <a className="underline font-blue-800" href="https://mega.nz/file/jf4h0DzA#hHGiaQW0oJsdcQfyi7EsyH4JnAzUZ_yc4XkZhwi5_rY" target="_blank" rel="noopener noreferrer">https://mega.nz/file/jf4h0DzA#hHGiaQW0oJsdcQfyi7EsyH4JnAzUZ_yc4XkZhwi5_rY</a></p>
+					{/* <Route exact path="/">
 						<Search />
 					</Route>
 					<Route path="/subreddit">
@@ -20,7 +24,7 @@ function App(props: any): ReactElement {
 					</Route>
           <Route path="/ep/:ep">
             <Ep />
-          </Route>
+          </Route> */}
 				</div>
 			</Router>
 		</React.Fragment>
